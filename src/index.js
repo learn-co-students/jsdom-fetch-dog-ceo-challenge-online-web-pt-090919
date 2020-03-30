ea16d811cf9c194223606723fa13ab40b7c6150b
github username <github email address>
2020-03-30 17:26:57 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+function getImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImages(json));
+}
+
+function renderImages(json){
+    let imgBox = document.querySelector("div#dog-image-container");
+    json.message.forEach(image_url => {
+        imgTag = document.createElement("img");
+        imgTag.setAttribute("src", `${image_url}`);
+        imgBox.appendChild(imgTag);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    getImages();
+});
