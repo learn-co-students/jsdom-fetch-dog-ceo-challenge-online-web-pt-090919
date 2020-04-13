9ebf791f8353e847de354a21d571e2c10ebc888a
kalmegati <kalmegati@gmail.com>
2020-04-13 10:58:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks()
+});
+
+function fetchBooks() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json){
+      renderBooks(json);
+    });
+};
+
+function renderBooks(json) {
+    const body = document.querySelector('body')
+    json["message"].forEach(element => {
+        let img = document.createElement('img')
+        img.src = `${element}`
+        body.appendChild(img)
+    });
+};
